d12a22b14a9a554ce6cf9057cf19c02b64e62a9a
sidizawi <zawisidim@gmail.com>
2021-01-04 01:51:50 +0100

    first commit

---
+++ android/app/src/main/java/com/sidi/dicee_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.sidi.dicee_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Dicee')
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
+  void changeState() {
+    setState(() {
+      leftDiceNum = 1 + Random().nextInt(6);
+      rightDiceNum = 1 + Random().nextInt(6);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Row(
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () => changeState(),
+                  child: Image.asset('images/dice$leftDiceNum.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () => changeState(),
+                  child: Image.asset('images/dice$rightDiceNum.png'),
+                ),
+              ),
+            ],
+          ),
+        );
+  }
+}
